2cacf40a7a7eabaed9f893ac73f173d6e0badf68
egr344 <43443460+egr344@users.noreply.github.com>
2021-08-06 13:19:33 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,46 +1,141 @@
 import logging
 import time
-from opcua import ua,Client
+from opcua import Client
 from threading import Thread
+from PyQt5 import QtWidgets, QtCore
+from window import Ui_MainWindow
+import sys
+
 
 class ParamWriter(Thread):
-    def __init__(self,sensorsAndServer):
+    # Конструктор класса. sensorsAndServer - список узлов и серверов, listLcdNumber - словарь где ключ это номер узла, а значени список lcd экранов для этого узла
+    def __init__(self, sensorsAndServer, dictLcdNumber):
         Thread.__init__(self)
         self.sensorsAndServer = sensorsAndServer
         self.stopMark = False
+        self.dictLcdNumber = dictLcdNumber
+
+# Функция меняет значения марекра остановки
     def stop(self):
         self.stopMark = True
+
+# функция передает значения температуры и вреимени с сервера на lcd дисплеи
     def run(self):
+        dict_node = self.get_dict_node()
         while not self.stopMark:
-            with open("test.txt", "a") as myfile:
-                myfile.write("\n")
-                for i in range(1,len(sensorsAndServer),1):
-                    myfile.write("Sensor {}\n".format(i))
-                    for param in sensorsAndServer[i].get_variables():
-                        myfile.write("{1}:{2:.2f} ".format(i,param.get_description().to_string(),param.get_value()))
-                    myfile.write("\n")
-            time.sleep(30)
+            for sensor in self.dictLcdNumber.keys():
+                self.dictLcdNumber[sensor][0].display(
+                    dict_node[sensor]["Tempreture"].get_value())
+                self.dictLcdNumber[sensor][1].display(
+                    dict_node[sensor]["Time"].get_value())
+            time.sleep(0.1)
 
-try:
-    from IPython import embed
-except ImportError:
-    import code
+# Возвращает словрь в котором каждому узлу соответсвтует словарь с наименованием значения и самим значением
+    def get_dict_node(self):
+        dict = {}
+        for i in range(1, len(self.sensorsAndServer), 1):
+            dict[i] = {}
+            for param in self.sensorsAndServer[i].get_variables():
+                if(param.get_description().to_string() == "Tempreture"):
+                    dict[i].update({"Tempreture": param})
+                elif(param.get_description().to_string() == "Time"):
+                    dict[i].update({"Time": param})
+        return dict
 
-    def embed():
-        vars = globals()
-        vars.update(locals())
-        shell = code.InteractiveConsole(vars)
-        shell.interact()
 
-logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.WARN)
-client = Client("opc.tcp://localhost:4840")
-try:
-    client.connect()
-    idx = client.get_namespace_index("MyServerOPC")
-    sensorsAndServer = client.get_root_node().get_children()[0].get_children()
-    pm = ParamWriter(sensorsAndServer)
-    pm.start()
-    embed()
-finally:
-    pm.stop()
-    client.disconnect()
+class mywindow(QtWidgets.QMainWindow):
+    # Конструктор класса. В нем задаются взаимосвязи кнопок и функций
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.connectFlag = False
+        self.ui.pushButtonConnect.clicked.connect(self.connect_click)
+        self.ui.pushButton_3.clicked.connect(self.set_velocity_force_sen_1)
+        self.ui.pushButton.clicked.connect(self.set_velocity_force_sen_2)
+        self.ui.pushButton_2.clicked.connect(self.set_velocity_force_sen_3)
+        self.ui.pushButton_5.clicked.connect(self.exit_program)
+
+    # функция запускает соединение с сервером
+    def connect_click(self):
+        if(not self.connectFlag):
+            logging.basicConfig(
+                level=logging.WARN)
+            self.client = Client("opc.tcp://localhost:4840")
+            try:
+                self.client.connect()
+                self.connectFlag = True
+                self.sensorsAndServer = self.client.get_root_node().get_children()[
+                    0].get_children()
+                self.pm = ParamWriter(self.sensorsAndServer, {
+                    1: [self.ui.lcdNumberTempSen1, self.ui.lcdNumberTimeSen1],
+                    2: [self.ui.lcdNumberTempSen2, self.ui.lcdNumberTimeSen2],
+                    3: [self.ui.lcdNumberTempSen3, self.ui.lcdNumberTimeSen3]})
+                self.pm.start()
+                self.ui.statusbar.showMessage("Соединение установлено")
+                self.ui.pushButtonConnect.setText("Disconnect")
+            except ConnectionRefusedError:
+                self.ui.statusbar.showMessage("Сервер не отвечает")
+        else:
+            self.connectFlag = False
+            self.pm.stop()
+            time.sleep(0.05)
+            self.client.disconnect()
+            self.ui.statusbar.showMessage("Соединение разорвано")
+            self.ui.pushButtonConnect.setText("Connect")
+
+# Записывает значения скорости и силы на сервер для первого сенсора
+    def set_velocity_force_sen_1(self):
+        if(self.connectFlag and self.ui.lineEdit.hasAcceptableInput() and
+           self.ui.lineEdit_4.hasAcceptableInput()):
+            for param in self.sensorsAndServer[1].get_variables():
+                if(param.get_description().to_string() == "Velocity"):
+                    param.set_value(self.ui.lineEdit.text())
+                elif(param.get_description().to_string() == "Force"):
+                    param.set_value(self.ui.lineEdit_4.text())
+            self.ui.statusbar.showMessage("Параметры установлены")
+        else:
+            self.ui.statusbar.showMessage(
+                "Параметры введены неверно или отсутствует подключение к серверу")
+
+# Записывает значения скорости и силы на сервер для второго сенсора
+    def set_velocity_force_sen_2(self):
+        if(self.connectFlag and self.ui.lineEdit_2.hasAcceptableInput() and
+           self.ui.lineEdit_5.hasAcceptableInput()):
+            for param in self.sensorsAndServer[2].get_variables():
+                if(param.get_description().to_string() == "Velocity"):
+                    param.set_value(self.ui.lineEdit_2.text())
+                elif(param.get_description().to_string() == "Force"):
+                    param.set_value(self.ui.lineEdit_5.text())
+            self.ui.statusbar.showMessage("Параметры установлены")
+        else:
+            self.ui.statusbar.showMessage(
+                "Параметры введены неверно или отсутствует подключение к серверу")
+
+# Записывает значения скорости и силы на сервер для третьего сенсора
+    def set_velocity_force_sen_3(self):
+        if(self.connectFlag and self.ui.lineEdit_3.hasAcceptableInput() and
+           self.ui.lineEdit_6.hasAcceptableInput()):
+            for param in self.sensorsAndServer[3].get_variables():
+                if(param.get_description().to_string() == "Velocity"):
+                    param.set_value(self.ui.lineEdit_3.text())
+                elif(param.get_description().to_string() == "Force"):
+                    param.set_value(self.ui.lineEdit_6.text())
+            self.ui.statusbar.showMessage("Параметры установлены")
+        else:
+            self.ui.statusbar.showMessage(
+                "Параметры введены неверно или отсутствует подключение к серверу")
+
+# Корректно завершает работу программы
+    def exit_program(self):
+        if (not self.connectFlag):
+            QtCore.QCoreApplication.instance().quit()
+        else:
+            self.connect_click()
+            QtCore.QCoreApplication.instance().quit()
+
+
+app = QtWidgets.QApplication([])
+application = mywindow()
+application.show()
+sys.exit(app.exec())
--- server.py
+++ server.py
@@ -1,49 +1,54 @@
-import sys
 import time
 import random
 import logging
-from opcua import ua,uamethod,Server
+from opcua import Server
 from threading import Thread
 
+
 class VarUpdater(Thread):
-    def __init__(self,temp,velocity,timeWork):
+    def __init__(self, temp, timeWork):
         Thread.__init__(self)
         self.stopMark = False
         self.temp = temp
-        self.velocity = velocity
         self.timeWork = timeWork
 
+    # Функция меняет значения марекра остановки
     def stop(self):
         self.stopMark = True
 
+    # Функция устанавливает значения для температуры и времени работы
     def run(self):
         startTime = time.time()
         while not self.stopMark:
-            self.temp.set_value(random.randint(56,60))
-            self.velocity.set_value(random.uniform(0,90))
-            self.timeWork.set_value(time.time() - startTime)
+            self.temp.set_value(random.randint(56, 60))
+            self.timeWork.set_value("{0:.0f}:{1:02.0f}:{2:02.0f}".format(
+                (time.time() - startTime)//3600,
+                (time.time() - startTime)//60 % 60,
+                (time.time() - startTime) % 60))
             time.sleep(0.1)
+
 
 try:
     from IPython import embed
 except ImportError:
     import code
-
+    # запускает интерактивную консоль
     def embed():
         myvars = globals()
         myvars.update(locals())
         shell = code.InteractiveConsole(myvars)
         shell.interact()
 
-
-def stopServer():
+# Останавливает работу сервера и работу потоков, в которых устанавливается значение температуры и времени
+def stop_server():
     sensor_1Updater.stop()
     sensor_2Updater.stop()
     sensor_3Updater.stop()
     server.stop()
     print("The server has stopped")
 
-def startServer():
+# Запускает работу сервера и работу потоков, в которых устанавливается значение температуры и времени
+def start_server():
     server.start()
     sensor_1Updater.start()
     sensor_2Updater.start()
@@ -51,38 +56,45 @@ def startServer():
     print("The server is up")
 
 
-logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.ERROR)
+logging.basicConfig(
+    format=u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+    level=logging.ERROR)
 server = Server()
 server.set_endpoint("opc.tcp://localhost:4840")
-server.set_security_policy([
-                ua.SecurityPolicyType.NoSecurity,
-                ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt,
-                ua.SecurityPolicyType.Basic256Sha256_Sign])
 
 uri = "MyServerOPC"
 idx = server.register_namespace(uri)
 node = server.get_objects_node()
 
-sensor_1 = node.add_object(idx,"Sensor_1")
-temp_s1 = sensor_1.add_variable(idx,"Tempreture",0)
-velocity_s1 = sensor_1.add_variable(idx,"Velocity",0)
-time_s1 = sensor_1.add_variable(idx,"Time",0)
+sensor_1 = node.add_object(idx, "Sensor_1")
+temp_s1 = sensor_1.add_variable(idx, "Tempreture", 0)
+velocity_s1 = sensor_1.add_variable(idx, "Velocity", 0)
+velocity_s1.set_writable()
+force_s1 = sensor_1.add_variable(idx, "Force", 0)
+force_s1.set_writable()
+time_s1 = sensor_1.add_variable(idx, "Time", 0)
 
-sensor_2 = node.add_object(idx,"Sensor_2")
-temp_s2 = sensor_2.add_variable(idx,"Tempreture",0)
-velocity_s2 = sensor_2.add_variable(idx,"Velocity",0)
-time_s2 = sensor_2.add_variable(idx,"Time",0)
+sensor_2 = node.add_object(idx, "Sensor_2")
+temp_s2 = sensor_2.add_variable(idx, "Tempreture", 0)
+velocity_s2 = sensor_2.add_variable(idx, "Velocity", 0)
+velocity_s2.set_writable()
+force_s2 = sensor_2.add_variable(idx, "Force", 0)
+force_s2.set_writable()
+time_s2 = sensor_2.add_variable(idx, "Time", 0)
 
-sensor_3 = node.add_object(idx,"Sensor_3")
-temp_s3 = sensor_3.add_variable(idx,"Tempreture",0)
-velocity_s3 = sensor_3.add_variable(idx,"Velocity",0)
-time_s3 = sensor_3.add_variable(idx,"Time",0)
+sensor_3 = node.add_object(idx, "Sensor_3")
+temp_s3 = sensor_3.add_variable(idx, "Tempreture", 0)
+velocity_s3 = sensor_3.add_variable(idx, "Velocity", 0)
+velocity_s3.set_writable()
+force_s3 = sensor_3.add_variable(idx, "Force", 0)
+force_s3.set_writable()
+time_s3 = sensor_3.add_variable(idx, "Time", 0)
 
-sensor_1Updater = VarUpdater(temp_s1,velocity_s1,time_s1)
-sensor_2Updater = VarUpdater(temp_s2,velocity_s2,time_s2)
-sensor_3Updater = VarUpdater(temp_s3,velocity_s3,time_s3)
+sensor_1Updater = VarUpdater(temp_s1, time_s1)
+sensor_2Updater = VarUpdater(temp_s2, time_s2)
+sensor_3Updater = VarUpdater(temp_s3, time_s3)
 try:
-    startServer()
+    start_server()
     embed()
 finally:
-    stopServer()
+    stop_server()
